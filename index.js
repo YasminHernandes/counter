8f4b5248ad580db51d76e1e186bfcd949568dad8
yasminhernandes <yasminhernandesjobs@gmail.com>
2022-01-24 16:26:01 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+(() => {
+    const minusButton = document.querySelector(".minus-button")
+    const plusButton = document.querySelector(".plus-button")
+    const counterValue = document.querySelector(".counter-value")
+    const resetButton = document.querySelector(".reset")
+
+    counterButtonHandler = operation => {
+        let counterCurrentValue = +counterValue.innerHTML
+        switch (operation) {
+            case "-":
+                counterCurrentValue--
+                if(counterCurrentValue >= 0) {
+                    counterValue.innerHTML = counterCurrentValue
+                }
+                break;
+            case "+":
+                counterCurrentValue++
+                counterValue.innerHTML = counterCurrentValue
+                break;
+            case "*":
+                counterValue.innerHTML = 0
+            default:
+                break;
+        }
+    }
+
+    minusButton.addEventListener('click', () => counterButtonHandler("-"))
+    plusButton.addEventListener('click', () => counterButtonHandler("+"))    
+    resetButton.addEventListener('click', () => counterButtonHandler("*"))    
+
+
+})()
